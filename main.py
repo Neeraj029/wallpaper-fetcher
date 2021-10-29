acb3329e0269465d6e303dfda2b7200854826ecc
Neeraj029 <neeraj.samshette@gmail.com>
2021-10-29 19:22:24 +0000

    some minor changes

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from urllib.request import urlopen
-from io import BytesIO
-from PIL import Image
-import time
-import os
-import re
-from urllib.request import urlopen, Request
-from gi.repository import Gtk, GdkPixbuf
-import requests
-from bs4 import BeautifulSoup
 import gi
 gi.require_version('Gtk', '3.0')
-
+from gi.repository import Gtk,GdkPixbuf
 
 class Window(Gtk.Window):
     def __init__(self):
@@ -26,10 +16,8 @@ class Window(Gtk.Window):
         # entry with clear and search icon
         entry = Gtk.Entry()
         entry.set_placeholder_text("Search")
-        entry.set_icon_from_icon_name(
-            Gtk.EntryIconPosition.PRIMARY, "edit-find-symbolic")
-        entry.set_icon_from_icon_name(
-            Gtk.EntryIconPosition.SECONDARY, "edit-clear-symbolic")
+        entry.set_icon_from_icon_name(Gtk.EntryIconPosition.PRIMARY, "edit-find-symbolic")
+        entry.set_icon_from_icon_name(Gtk.EntryIconPosition.SECONDARY, "edit-clear-symbolic")
         self.set_titlebar(self.header)
         self.header.pack_start(entry)
         self.add(self.header)
@@ -38,10 +26,10 @@ class Window(Gtk.Window):
 
         # show image from its path
         pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(
-            filename="/home/neeraj/Desktop/tree.png",
-            width=1000,
-            height=1000,
-            preserve_aspect_ratio=True)
+        filename="./download.jpeg", 
+        width=1000, 
+        height=1000, 
+        preserve_aspect_ratio=True)
 
         self.image = Gtk.Image.new_from_pixbuf(pixbuf)
         # decrease image size
@@ -50,16 +38,27 @@ class Window(Gtk.Window):
         print(self.image.set_from_pixbuf)
         self.add(self.image)
 
+
+
+
     def on_icon_click(self, widget, icon_pos, event):
         if(icon_pos == Gtk.EntryIconPosition.SECONDARY):
             widget.set_text("")
         else:
             # get text from entry and search wallpaper at wallhaven.cc
             text = widget.get_text()
+            from urllib.request import urlopen,Request  
+            from bs4 import BeautifulSoup
+            import requests
+            import re
+            import os
+            import time
+            from PIL import Image
+            from io import BytesIO
+            from urllib.request import urlopen
 
             # get the url of the image
-            url = "https://wallhaven.cc/search?q=" + text + \
-                "&categories=110&purity=100&sorting=random&order=desc"
+            url = "https://wallhaven.cc/search?q=" + text + "&categories=110&purity=100&sorting=random&order=desc"
             # get the html of the page
             print(url)
             page = requests.get(url)
@@ -89,12 +88,14 @@ class Window(Gtk.Window):
             # save the image
             image.save(image_path)
             # change self.image image to the downloaded image
-
+            
             self.image.set_from_file(image_path)
 
-    def on_button_clicked(self, widget):
-        print("Hello World")
 
+
+
+    def on_button_clicked(self, widget):
+        print("Hello World") 
 
 window = Window()
 window.connect("delete-event", Gtk.main_quit)
